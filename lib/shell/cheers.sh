baf399eb4dabad847c441d8c2017e19149b81068
ptsakyrellis <philippe.tsakyrellis@gmail.com>
2015-08-23 23:37:51 -0500

    added shell script capacity, config file in JSON format to be used instead of JS config.
    updated syntax consistency in the cheers.js file

---
+++ lib/shell/cheers.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var cheers = require('../cheers'), args = process.argv.slice(2), config = {};
+
+if(args[0] == '-conf')
+{
+   try
+   {
+    config = require(args[1]);
+   }
+   catch(Exception)
+   {
+    console.log(Exception);
+    process.exit();
+   }
+}
+else
+{
+   console.error('Please provide the path to the config file to use using -conf option.');
+   process.exit();
+}
+
+// For some reason the Regexp string of the config files
+// has trouble being seen as a RegExp object
+for(var key in config.scrape){
+    var mapping = config.scrape[key];
+    if(mapping['extract'] != undefined)
+    {
+        var regParts = mapping['extract'].match(/^\/(.*?)\/([gim]*)$/);
+        if (regParts) {
+            mapping['extract'] = new RegExp(regParts[1], regParts[2]);
+        }
+    }
+}
+
+cheers.scrape(config).then(function (results) {
+    console.log(JSON.stringify(results));
+}).catch(function (error) {
+    console.error(error);
+});
